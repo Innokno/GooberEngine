3e3d3b5790ce1f365c4ebb35cfa3a3ac5c3505c9
Innokno <niksoer@outlook.com>
2025-02-07 17:32:40 +0100

    Changed the way the build file is stored

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
 #include <cstdint>
 #include <iostream>
 
@@ -15,40 +21,69 @@ int main() {
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
-			WIDTH, HEIGHT);
+	SDL_Texture* mainGameTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 
+			WIDTH, HEIGHT); 
 
 	bool running = true;
 
-//	uint32_t* pixels = new uint32_t[WIDTH*HEIGHT];
+	int currentTime = 0;
+	int previousTime = 0;
+
+	float distance = 0;
+	float deltaTime = 0; 
+
+	uint32_t* pixels = new uint32_t[WIDTH*HEIGHT];
 
 	SDL_Event event;
 
 	while (running) {
 		
 		while (SDL_PollEvent(&event)) {
-
-			std::cout << SDL_QUIT << std::endl;
-
 			if (event.type == SDL_QUIT) {
 				running = false;
+
 			}
+
+			else if (event.type == SDL_KEYDOWN) {
+
+				switch (event.key.keysym.sym) {
+					
+					case SDLK_ESCAPE:
+						running = false;
+						break;
+					case SDLK_w:
+						distance += 100 * deltaTime;
+						break;
+					case SDLK_s:
+						distance -= 100 * deltaTime;
+						break;
+				}
+			}
+		}
+
+		for (int i = 0; i < WIDTH; i++) {
+
+			pixels[WIDTH * i + (WIDTH / 2)] = SDL_MapRGBA(SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888), 
+					255, 255, 255, 255);
 
 		}
 
-		 SDL_SetRenderDrawColor(renderer, 255, 155, 0, 100);
-		
-//		 SDL_UpdateTexture(texture, nullptr, pixels, WIDTH * sizeof(uint32_t));
+		std::cout << distance << std::endl;
 
-     SDL_RenderClear(renderer);
-		 SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-     SDL_RenderPresent(renderer);
+		SDL_UpdateTexture(mainGameTexture, nullptr, pixels, WIDTH * sizeof(uint32_t));	
+		SDL_RenderCopy(renderer, mainGameTexture, nullptr, nullptr);
+
+    SDL_RenderPresent(renderer);
+
+		currentTime = SDL_GetTicks();
+		deltaTime = (currentTime - previousTime) / 1000.0f;
+		previousTime = currentTime;
 	}
 
-//	 delete[] pixels;
-	 SDL_DestroyTexture(texture);
-   SDL_DestroyRenderer(renderer);
- 	 SDL_DestroyWindow(window);
-   SDL_Quit();
-   return 0;
+	delete[] pixels;
+	SDL_DestroyTexture(mainGameTexture);
+  SDL_DestroyRenderer(renderer);
+ 	SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
